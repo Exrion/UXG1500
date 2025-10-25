04735dc0e36ea570fd85a205f1ba828a8409eedc
Cai Xuan <exrion.lexom@gmail.com>
2025-10-25 17:16:13 +0800

    basic player prefab added

---
+++ UXG1500_Unity/Assets/src/core/SceneController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private Transform m_PlayerTransform;
+    [SerializeField] private Transform m_CameraTransform;
+
+    void Update()
+    {
+        
+    }
+
+    void RecalculateCameraDirection()
+    {
+
+    }
+}
