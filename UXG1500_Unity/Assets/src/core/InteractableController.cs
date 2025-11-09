4957f4b1cdc572bdf24717f91345aa7269ff681a
Cai Xuan <exrion.lexom@gmail.com>
2025-11-09 13:20:58 +0800

    added scripts for interaction (incomplete)

--- UXG1500_Unity/Assets/src/core/InteractableController.cs
+++ UXG1500_Unity/Assets/src/core/InteractableController.cs
@@ -1,16 +1,42 @@
+using System;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InteractableController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private float m_InteractionDistance;
+    [SerializeField]
+    private Camera m_Camera;
+    [SerializeField]
+    private string m_InteractActionName;
+
+    private InputAction m_InteractAction;
+    private Stopwatch m_Stopwatch;
+
+    private void Start()
     {
-        
+        m_InteractAction = InputSystem.actions.FindAction(m_InteractActionName);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (m_InteractAction != null)
+            CheckInteract();
+    }
+
+    private void CheckInteract()
+    {
+        if (m_InteractAction.WasReleasedThisFrame())
+        {
+            CastToInteractable();
+            
+        }
+    }
+
+    private void CastToInteractable()
+    {
+
     }
 }
