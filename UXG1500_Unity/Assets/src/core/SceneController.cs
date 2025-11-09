1c82472292a0ee49b364d1ce7fc98a24b4f8e8b0
Cai Xuan <exrion.lexom@gmail.com>
2025-11-09 20:16:11 +0800

    Completed Scene 2 tolerably

--- UXG1500_Unity/Assets/src/core/SceneController.cs
+++ UXG1500_Unity/Assets/src/core/SceneController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
