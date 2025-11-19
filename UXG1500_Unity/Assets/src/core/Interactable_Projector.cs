4201ab7791404c1bc44331c9bf1a1a2f010f8567
Cai Xuan <exrion.lexom@gmail.com>
2025-11-19 12:41:59 +0800

    Projector basic interaction

--- UXG1500_Unity/Assets/src/core/Interactable_Projector.cs
+++ UXG1500_Unity/Assets/src/core/Interactable_Projector.cs
@@ -1,15 +1,45 @@
 using System.Collections.Generic;
 using UnityEngine.UIElements;
 using UnityEngine;
+using UnityEditor;
 
 class Interactable_Projector : IInteractable
 {
     [SerializeField]
-    private List<UIDocument> m_Slides = new();
+    List<string> m_SlidePaths = new();
+    TemplateContainer[] m_Slides;
+    int m_SlideCount;
 
+    protected override void Start()
+    {
+        base.Start();
+        InitSlides();
+        UpdateSlides();
+    }
 
     public override void OnInteracted()
     {
-        throw new System.NotImplementedException();
+        AdvanceSlides();
+        UpdateSlides();
+    }
+
+    void AdvanceSlides() => m_SlideCount = m_SlideCount >= m_Slides.Length - 1 ? 0 : m_SlideCount + 1;
+    void InitSlides() => m_Slides = new TemplateContainer[m_SlidePaths.Count];
+
+    bool CreateSlide(string path, out TemplateContainer slide)
+    {
+        slide = null;
+        VisualTreeAsset va = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(path);
+        if (va != null)
+        {
+            slide = va.CloneTree();
+            return true;
+        }
+        return false;
+    }
+
+    void UpdateSlides()
+    {
+
     }
 }
